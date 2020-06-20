5ffda4ee1b1536cd473ed9fc19839e6b58ffc6ed
Cheglok <Kopytovmikhail@yandex.ru>
2020-06-20 20:42:06 +0500

    start

---
+++ script.js
@@ -0,0 +1,31 @@
+if (!localStorage.getItem("question")) {
+  localStorage.setItem("question", 1);
+}
+console.log(localStorage.getItem("question"));
+let form = document.forms.answer_form;
+
+form.user_answer.oninput = check;
+let question = document.getElementById("question");
+let bingo = document.getElementById("bingo");
+console.log(question);
+let question_array = [
+  "Давай просто прогуляемся по паркам! Хотя постойте-ка, что это за странные деревья ? Тут картинка",
+  "Вопрос два",
+  "Вопрос три",
+];
+
+function check() {
+  let user_answer = form.user_answer.value;
+  if (user_answer == "3" || user_answer == "три") {
+    bingo.innerHTML =
+      "Точно, три штуки! Ещё утром было два. Никому не говори, это смертельно опасные хищники, триффиды! Возможно что-то не так с этим миром.";
+    question.innerHTML = question_array[1];
+    localStorage.setItem("question", 2);
+    console.log(localStorage.getItem("question"));
+  }
+  if (user_answer == "bbb") {
+    question.innerHTML = question_array[2];
+    localStorage.setItem("question", 3);
+    console.log(localStorage.getItem("question"));
+  }
+}
